ad62083000f12b41ddd6126fc85bb1757fe134df
Utshab Kumar Ghosh <30478569+utshab-1603022@users.noreply.github.com>
2020-06-06 18:38:41 +0600

    1027 in Python
    Caution: LIGHTOJ doesn't accept Python

---
+++ LIGHTOJ/Python/1027.py
@@ -0,0 +1,113 @@
+from __future__ import division, print_function
+from sympy import symbols, Eq, solve
+# import threading
+# threading.stack_size(2**27)
+# import sys
+# sys.setrecursionlimit(10**7)
+# sys.stdin = open('inpy.txt', 'r')
+# sys.stdout = open('outpy.txt', 'w')
+from sys import stdin, stdout
+import bisect            #c++ upperbound
+import math
+import heapq
+i_m=9223372036854775807
+def inin():
+    return int(input())
+def stin():
+    return input()
+def spin():
+    return map(int,stin().split())
+def lin():                           #takes array as input
+    return list(map(int,stin().split()))
+#######################################
+def modinv(n,p):
+    return pow(n,p-2,p)
+
+def GCD(x, y): 
+    x=abs(x)
+    y=abs(y)
+    if(min(x,y)==0):
+        return max(x,y)
+    while(y): 
+        x, y = y, x % y 
+    return x
+def Divisors(n) : 
+    l = []  
+    for i in range(1, int(math.sqrt(n) + 1)) :
+        if (n % i == 0) : 
+            if (n // i == i) : 
+                l.append(i) 
+            else : 
+                l.append(i)
+                l.append(n//i)
+    return l
+prime=[]
+def SieveOfEratosthenes(n): 
+    global prime
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+        if (prime[p] == True): 
+            for i in range(p * p, n+1, p): 
+                prime[i] = False
+        p += 1
+    f=[]
+    for p in range(2, n): 
+        if prime[p]: 
+            f.append(p)
+    return f
+q=[]       
+def dfs(n,d,v,c):
+    global q
+    v[n]=1
+    x=d[n]
+    q.append(n)
+    j=c
+    for i in x:
+        if i not in v:
+            f=dfs(i,d,v,c+1)
+            j=max(j,f)
+            # print(f)
+    return j
+def gcd(x ,y):
+    while(y):
+        t = x % y
+        x = y
+        y = t
+    
+    return x
+# d = {}
+ 
+"""*******************************************************"""
+
+# If you choose one of the negative doors, 
+# it will take you some time ti to get back to the same spot, 
+# and because there is no memory, you are back to needing an expected E minutes to escape again
+
+# E = 1/3 * (3 + (6 + E) + (9+E))
+
+for j in range(inin()):
+    n = inin()
+    l = lin()
+
+    p = 1/n
+    exp = 0
+    E = symbols('E')
+    for i in l:
+        if i>0:
+            exp += p*i
+        else:
+            exp += p*(abs(i)+E)
+
+        eq = Eq(E-exp, 0)
+    sol = solve(eq)
+    if len(sol)==0 or sol[0] == float("+inf"):
+        print("Case %d: inf" % (j+1))
+    else:
+        from fractions import Fraction
+        print("Case %d: "%(j+1), end='')
+        if Fraction(str(sol[0]))-math.fabs(Fraction(str(sol[0])))==0:
+            print(Fraction(str(sol[0])), end='')
+            print("/1")
+        else:
+            print(Fraction(str(sol[0])))
