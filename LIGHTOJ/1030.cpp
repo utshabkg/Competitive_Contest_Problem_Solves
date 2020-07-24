6a1d67ca605fef8f17c6b41cba712614afdb6072
Utshab Kumar Ghosh <30478569+utshab-1603022@users.noreply.github.com>
2020-07-24 20:15:37 +0600

    Create 1030.cpp

---
+++ LIGHTOJ/1030.cpp
@@ -0,0 +1,52 @@
+//#pragma GCC optimize("Ofast,unroll-loops")
+
+//#pragma GCC target("avx,avx2,fma")
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/***template***/
+#define ll                   long long
+#define dd                   double
+#define scl(n)               scanf("%lld",&n)
+#define scd(n)               scanf("%lf",&n)
+#define pi                   pair<ll,ll>
+#define pb                   push_back
+#define mp                   make_pair
+#define uu                   first
+#define vv                   second
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define FOOR(i,a,b)          for(ll i=a;i<=b;i++)
+#define LOP(i,a,b)           for(ll i=a;i<b;i++)
+#define sorted(s)            sort(s.begin(),s.end())
+#define reversed(s)          reverse(s.begin(),s.end())
+#define contains(a,b)        (find((a).begin(), (a).end(), (b)) != (a).end())
+#define MAXN                 10004
+#define pii                  3.1415926536
+#define mod                  1000000007
+#define eps                  0.0000000001
+#define inf                  9000000000000000
+#define mstt(a,b)            memset((a),(b),sizeof (a))
+
+#define base1                1000002089
+#define base2                1000003853
+#define hashmod              1000002989
+
+#define FastRead             ios_base::sync_with_stdio(0);cin.tie(nullptr);
+
+/*
+ll getsegmenthash(int x, int y)
+{
+    return ((hsh[y] - pw1[y - x + 1] * hsh[x - 1]) % hashmod + hashmod ) % hashmod;
+}
+*/
+
+//priority_queue<ll, vector<ll>, greater<ll> >q
+
+int main()
+{
+    
+}
