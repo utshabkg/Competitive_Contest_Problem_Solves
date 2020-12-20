61e7a5b7cf10d9a8245f194cb29e221503e71eca
Utshab Kumar Ghosh <kumarutshab@gmail.com>
2020-12-20 13:22:14 +0600

    up solve

---
+++ CODEFORCES/Python/691 (Div. 2)/A.py
@@ -0,0 +1,228 @@
+from __future__ import division, print_function
+from itertools import permutations, combinations
+# import threading
+# threading.stack_size(2**27)
+# import sys
+# sys.setrecursionlimit(10**7)
+# sys.stdin = open('inpy.txt', 'r')
+# sys.stdout = open('outpy.txt', 'w')
+from sys import stdin, stdout
+import bisect  # c++ upperbound
+import math
+import heapq
+i_m = 9223372036854775807
+
+
+def inin():
+    return int(input())
+
+
+def stin():
+    return input()
+
+
+def spin():
+    return map(int, stin().split())
+
+
+def lin():  # takes array as input
+    return list(map(int, stin().split()))
+
+
+def matrix(n):
+    #matrix input
+    return [list(map(int, input().split()))for i in range(n)]
+
+################################################
+
+
+def string2intlist(s):
+    return list(map(int, s))
+
+
+def calculate_sum(a, N):  # sum of a to N
+    # Number of multiples
+    m = N / a
+    # sum of first m natural numbers
+    sum = m * (m + 1) / 2
+    # sum of multiples
+    ans = a * sum
+    return ans
+
+
+def series(N):
+    return (N*(N+1))//2
+
+
+def count2Dmatrix(i, list):
+    return sum(c.count(i) for c in list)
+
+
+def modinv(n, p):
+    return pow(n, p - 2, p)
+
+
+def nCr(n, r):
+    i = 1
+    while i < r:
+        n *= (n - i)
+        i += 1
+    return n // math.factorial(r)
+
+def LCM(x, y):
+    return (x * y) // math.gcd(x, y)
+
+
+def Divisors(n):
+    l = []
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if (n % i == 0):
+            if (n // i == i):
+                l.append(i)
+            else:
+                l.append(i)
+                l.append(n//i)
+    return l
+
+
+def isprime(n):
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(math.sqrt(n))+1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
+
+
+prime = []
+
+
+def SieveOfEratosthenes(n):
+    global prime
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    f = []
+    for p in range(2, n):
+        if prime[p]:
+            f.append(p)
+    return f
+
+
+q = []
+
+
+def dfs(n, d, v, c):
+    global q
+    v[n] = 1
+    x = d[n]
+    q.append(n)
+    j = c
+    for i in x:
+        if i not in v:
+            f = dfs(i, d, v, c+1)
+            j = max(j, f)
+            # print(f)
+    return j
+# d = {}
+
+
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+
+    return K[n][W]
+
+
+def modularExponentiation(x, n):
+    M = 10**9+7
+    if(n == 0):
+        return 1
+    elif (n % 2 == 0):  # n is even
+        return modularExponentiation((x*x) % M, n//2)
+    else:  # n is odd
+        return (x * modularExponentiation((x * x) % M, (n - 1) // 2)) % M
+
+
+def modInverse(a, m):
+    m0 = m
+    y = 0
+    x = 1
+
+    if (m == 1):
+        return 0
+
+    while (a > 1):
+
+        # q is quotient
+        q = a // m
+
+        t = m
+
+        # m is remainder now, process
+        # same as Euclid's algo
+        m = a % m
+        a = t
+        t = y
+
+        # Update x and y
+        y = x - q * y
+        x = t
+
+    # Make x positive
+    if (x < 0):
+        x = x + m0
+
+    return x
+
+
+"""*******************************************************"""
+for _ in range(inin()):
+    n = inin()
+    r = list(stin())
+    b = list(stin())
+    
+    # rl, bl = [], []
+    # for i in list(combinations(r, n)):
+    #     rl.append(int("".join(i)))
+    # for i in list(permutations(b)):
+    #     bl.append(int("".join(i)))
+
+    # print(rl, bl)
+
+    sr, sb = 0, 0
+    # for i in range(n):
+    #     sr += int(r[i])
+    #     sb += int(b[i])
+    for i in range(n):
+        if int(r[i]) > int(b[i]):
+            sr += 1
+        elif int(r[i]) < int(b[i]):
+            sb += 1
+
+    # sr = sum(list(int(r)))
+    # sb = sum(list(int(b)))
+
+    # print(sr, sb)
+    if sr > sb:
+        print("RED")
+    elif sr < sb:
+        print("BLUE")
+    else:
+        print("EQUAL")
+        
