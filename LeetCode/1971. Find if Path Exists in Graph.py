2de077f8ef6a778af4e3326811b31488985f62f3
utshabkg <ranjanyouth@gmail.com>
2022-12-03 02:40:26 +0600

    DFS with recursion. Path Traversal.

---
+++ LeetCode/1971. Find if Path Exists in Graph.py
@@ -0,0 +1,25 @@
+import collections
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        print(graph)
+        seen = [False] * n
+
+        def dfs(curr_node):
+            print(curr_node, seen)
+            if curr_node == destination:
+                return True
+            if not seen[curr_node]:
+                seen[curr_node] = True
+                for next_node in graph[curr_node]:
+                    if dfs(next_node):
+                        return True
+            return False
+
+        return dfs(source)
